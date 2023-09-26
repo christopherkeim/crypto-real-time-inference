b061a3aaef1a649cdc4f32534d3723f1057f2ce7
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-26 16:43:13 -0400

    Removed old components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,31 +1,15 @@
 export const metadata = {
-  title: "Home - Appy",
-  description: "Page description",
+  title: "Crypto Real Time Inference",
+  description: "Built by Chris Keim",
 };
 
-import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
-import Stats from "@/components/stats";
-import Carousel from "@/components/carousel";
-import Tabs from "@/components/tabs";
-import Process from "@/components/process";
-import PricingTables from "@/components/pricing-tables";
-import TestimonialsBlocks from "@/components/testimonials-blocks";
-import FeaturesBlocks from "@/components/features-blocks";
-import Cta from "@/components/cta";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
-      <Carousel />
-      <Tabs />
-      <Process />
-      <PricingTables />
-      <TestimonialsBlocks />
-      <FeaturesBlocks />
-      <Cta />
+      {/* More content here */}
     </>
   );
 }
