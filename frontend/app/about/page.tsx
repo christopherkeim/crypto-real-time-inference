ee46f9e8620526d514c08d5afe22fc2a8f695669
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-27 11:35:11 -0400

    Removed components causing errors

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,15 +1,13 @@
 export const metadata = {
-  title: 'About - Appy',
-  description: 'Page description',
-}
+  title: "About - Appy",
+  description: "Page description",
+};
 
-import Hero from '@/components/hero-about'
-import FeaturesGallery from '@/components/features-gallery'
-import Timeline from '@/components/timeline'
-import Career from '@/components/career'
-import FeaturesAnimation from '@/components/features-animation'
-import Team from '@/components/team'
-import CtaContact from '@/components/cta-contact'
+import Hero from "@/components/hero-about";
+import FeaturesGallery from "@/components/features-gallery";
+import Timeline from "@/components/timeline";
+import Career from "@/components/career";
+import Team from "@/components/team";
 
 export default function About() {
   return (
@@ -18,9 +16,7 @@ export default function About() {
       <FeaturesGallery />
       <Timeline />
       <Career />
-      <FeaturesAnimation />
       <Team />
-      <CtaContact />
     </>
-  )
+  );
 }
