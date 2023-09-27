7b3deef299fcec66cb4dc6fcb7464bf7f28eead0
Kenson Johnson <kenson.johnson@outlook.com>
2023-09-27 13:45:12 -0400

    Added "Research Paper" styling to content

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,16 @@
+import Hero from "@/components/hero-home";
+import { ResearchPaper } from "@/components/ResearchPaper/ResearchPaper";
+
 export const metadata = {
   title: "Crypto Real Time Inference",
   description: "Built by Chris Keim",
 };
 
-import Hero from "@/components/hero-home";
-
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* More content here */}
+      <ResearchPaper />
     </>
   );
 }
