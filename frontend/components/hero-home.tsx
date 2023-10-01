eaf8e8547803270e572308c63d9843a4feab5567
Kenson Johnson <kenson.johnson@outlook.com>
2023-10-01 13:54:27 -0500

    Changed to use PredictionContainer component

--- frontend/components/hero-home.tsx
+++ frontend/components/hero-home.tsx
@@ -1,17 +1,17 @@
-import { PredictionForm } from "./PredictionForm";
+import { PredictionContainer } from "./Prediction/PredictionContainer";
 
 export default function HeroHome() {
   return (
     <section>
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 md:px-12 lg:px-20 pt-32 pb-10 md:pt-40 md:pb-16 flex flex-col md:gap-12 lg:gap-20 items-center mb-8 text-left">
+      <div className="mx-auto mb-8 flex max-w-6xl flex-col items-center px-4 pb-10 pt-32 text-left sm:px-6 md:gap-12 md:px-12 md:pb-16 md:pt-40 lg:gap-20 lg:px-20">
         <h1
-          className="h1 lg:text-6xl font-red-hat-display font-black"
+          className="h1 font-red-hat-display font-black lg:text-6xl"
           data-aos="fade-down"
         >
           Crypto <span className="text-blue-500">Real Time</span>{" "}
           <span className="text-green-500">Inference</span>
         </h1>
-        <PredictionForm />
+        <PredictionContainer />
         <p
           className="text-xl text-gray-600 dark:text-gray-400"
           data-aos="fade-down"
