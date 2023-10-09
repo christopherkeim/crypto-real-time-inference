45410bd5b41ef8173d40e199523d8d201c0ebe31
Christopher Keim <christopher.jl.keim@gmail.com>
2023-10-09 18:00:23 -0400

    Script documentation.

--- deploy/deploy_container.sh
+++ deploy/deploy_container.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-# Spin up the Prediction server
+# Spin up the Prediction server if the container is not running; OR if the image on disk 
+# isn't up to date, pull the new image, tear down the running container, and spin up the
+# new container
 docker compose -f deploy/prediction-docker-compose.yml up --detach --pull predict
