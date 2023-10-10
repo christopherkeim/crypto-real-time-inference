7c499be01d3a13f7e6be40e995a74dfd25dc4660
Christopher Keim <christopher.jl.keim@gmail.com>
2023-10-10 18:17:47 -0400

    Add environment variable sub in curl.

--- deploy/deploy_webhook.sh
+++ deploy/deploy_webhook.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Deploys the webhook binary if it is not currently up
-if ( curl -X POST http://127.0.0.1:10000/infra/your-deploy-key-here > /dev/null )
+if ( curl -X POST http://127.0.0.1:10000/infra/$DOCKER_HUB_DEPLOY_KEY > /dev/null )
 then
   echo "Webhook server is running on http://127.0.0.1:10000/infra/ 🟢"
 else
