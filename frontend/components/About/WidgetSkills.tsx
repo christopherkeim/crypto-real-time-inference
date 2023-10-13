44f45f5e6949b957d988291a2ed6361b5750c65a
Kenson Johnson <kenson.johnson@outlook.com>
2023-10-13 12:35:32 -0500

    ProgressBar component fails in prod. Removing

--- frontend/components/About/WidgetSkills.tsx
+++ frontend/components/About/WidgetSkills.tsx
@@ -10,50 +10,65 @@ export function WidgetSkills() {
             <span className="mr-2 text-sky-500">—</span>{" "}
             <a className="text-sm font-[650]">Python</a>
           </div>
-          <ProgressBar
-            className="w-60 shrink-0 sm:w-80 md:w-20 lg:w-24"
-            value={90}
-          />
+          <div
+            className={`relative h-1.5 w-60 shrink-0 bg-slate-200 before:absolute before:inset-0 before:w-[90%] before:bg-sky-500 dark:bg-slate-700 sm:w-80 md:w-20 lg:w-24`}
+            role="progressbar"
+            aria-valuenow={90}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
         </li>
         <li className="flex items-center justify-between">
           <div className="mr-1 inline-flex grow">
             <span className="mr-2 text-sky-500">—</span>{" "}
             <a className="text-sm font-[650]">Bash/Shell</a>
           </div>
-          <ProgressBar
-            className="w-60 shrink-0 sm:w-80 md:w-20 lg:w-24"
-            value={60}
-          />
+          <div
+            className={`relative h-1.5 w-60 shrink-0 bg-slate-200 before:absolute before:inset-0 before:w-[60%] before:bg-sky-500 dark:bg-slate-700 sm:w-80 md:w-20 lg:w-24`}
+            role="progressbar"
+            aria-valuenow={60}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
         </li>
         <li className="flex items-center justify-between">
           <div className="mr-1 inline-flex grow">
             <span className="mr-2 text-sky-500">—</span>{" "}
             <a className="text-sm font-[650]">Machine Learning</a>
           </div>
-          <ProgressBar
-            className="w-60 shrink-0 sm:w-80 md:w-20 lg:w-24"
-            value={60}
-          />
+          <div
+            className={`relative h-1.5 w-60 shrink-0 bg-slate-200 before:absolute before:inset-0 before:w-[60%] before:bg-sky-500 dark:bg-slate-700 sm:w-80 md:w-20 lg:w-24`}
+            role="progressbar"
+            aria-valuenow={60}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
         </li>
         <li className="flex items-center justify-between">
           <div className="mr-1 inline-flex grow">
             <span className="mr-2 text-sky-500">—</span>{" "}
             <a className="text-sm font-[650]">Data Analytics</a>
           </div>
-          <ProgressBar
-            className="w-60 shrink-0 sm:w-80 md:w-20 lg:w-24"
-            value={60}
-          />
+          <div
+            className={`relative h-1.5 w-60 shrink-0 bg-slate-200 before:absolute before:inset-0 before:w-[60%] before:bg-sky-500 dark:bg-slate-700 sm:w-80 md:w-20 lg:w-24`}
+            role="progressbar"
+            aria-valuenow={60}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
         </li>
         <li className="flex items-center justify-between">
           <div className="mr-1 inline-flex grow">
             <span className="mr-2 text-sky-500">—</span>{" "}
             <a className="text-sm font-[650]">Go</a>
           </div>
-          <ProgressBar
-            className="w-60 shrink-0 sm:w-80 md:w-20 lg:w-24"
-            value={25}
-          />
+          <div
+            className={`relative h-1.5 w-60 shrink-0 bg-slate-200 before:absolute before:inset-0 before:w-[25%] before:bg-sky-500 dark:bg-slate-700 sm:w-80 md:w-20 lg:w-24`}
+            role="progressbar"
+            aria-valuenow={25}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
         </li>
       </ul>
     </section>
