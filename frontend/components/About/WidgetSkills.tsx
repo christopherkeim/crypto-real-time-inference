03a48a896ceb1d1018f00f6d7ff0edc79aeb4d81
Kenson Johnson <kenson.johnson@outlook.com>
2023-10-13 12:27:48 -0500

    Testing progress bar

--- frontend/components/About/WidgetSkills.tsx
+++ frontend/components/About/WidgetSkills.tsx
@@ -10,10 +10,13 @@ export function WidgetSkills() {
             <span className="mr-2 text-sky-500">—</span>{" "}
             <a className="text-sm font-[650]">Python</a>
           </div>
-          <ProgressBar
-            className="w-60 shrink-0 sm:w-80 md:w-20 lg:w-24"
-            value={90}
-          />
+          <div
+            className={`relative h-1.5 w-60 shrink-0 bg-slate-200 before:absolute before:inset-0 before:w-[90%] before:bg-sky-500 dark:bg-slate-700 sm:w-80 md:w-20 lg:w-24`}
+            role="progressbar"
+            aria-valuenow={90}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
         </li>
         <li className="flex items-center justify-between">
           <div className="mr-1 inline-flex grow">
