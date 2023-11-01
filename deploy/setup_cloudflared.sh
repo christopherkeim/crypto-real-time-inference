55496e9bbca6493197e117e8e9b2756be4f8be5a
Kenson Johnson <kenson.johnson@outlook.com>
2023-11-01 14:18:20 -0500

    Added setup for cloudflared

---
+++ deploy/setup_cloudflared.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#-----------------------------------------------------------------------------------------------------------#
+# This script sets up a deployment environment on an Ubuntu 20.04/22.04
+#
+# Documentation:
+# https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/get-started/create-local-tunnel/
+#
+#-----------------------------------------------------------------------------------------------------------#
+
+
+# Add Cloudflare’s package signing key
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+# Add Cloudflare’s apt repo to your apt repositories
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+# Update repositories and install cloudflared
+sudo apt-get update && sudo apt-get install cloudflared
