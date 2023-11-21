f89e7759df7489232a83eae5eebf84d028569684
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-21 17:29:45 -0600

    Stripped header and footer

--- frontend/components/ui/header.tsx
+++ frontend/components/ui/header.tsx
@@ -5,68 +5,9 @@ import MobileMenu from "./mobile-menu";
 
 export default function Header() {
   return (
-    <header className="absolute w-full z-30">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-20">
-          {/* Site branding */}
-          <div className="shrink-0 mr-5">
-            <Link
-              href="/"
-              className="block"
-              aria-label="Crypto Real Time Inference"
-            >
-              <Logo />
-            </Link>
-          </div>
-
-          {/* Desktop navigation */}
-          <nav className="hidden md:flex md:grow">
-            {/* Desktop menu links */}
-            <ul className="flex grow flex-wrap items-center font-medium">
-              <li>
-                <Link
-                  href="/"
-                  className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/about"
-                  className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/policy"
-                  className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 px-5 py-2 flex items-center transition duration-150 ease-in-out"
-                >
-                  Policy
-                </Link>
-              </li>
-            </ul>
-
-            {/* Desktop lights switch */}
-            <ThemeToggle className="ml-3" />
-
-            {/* Desktop CTA on the right */}
-            <ul className="flex justify-end flex-wrap items-center">
-              <li>
-                <Link
-                  href="/contact"
-                  className="btn-sm text-white bg-blue-800 hover:bg-blue-700 ml-6"
-                >
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-
-          <MobileMenu />
-        </div>
+    <header className="absolute z-30 w-full">
+      <div className="mx-auto flex max-w-6xl justify-start px-4">
+        <ThemeToggle className="ms-14 mt-3" />
       </div>
     </header>
   );
